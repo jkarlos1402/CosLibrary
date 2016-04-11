d7d5671182c4890e47a7e6b4fbde69f07e78da25
TMXIDSJPINAM <TMXIDSJPINAM@JCARLOS.na.kof.ccf>
2016-04-11 18:08:17 -0500

    se agregarron 2 catalogos

---
+++ src/com/cossystem/core/util/ManagerJSON.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cossystem.core.util;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * @author TMXIDSJPINAM
+ */
+public class ManagerJSON {
+    public static JSONObject getJSONObjectFromString(String jsonString){
+        return new JSONObject(jsonString);
+    }
+}
